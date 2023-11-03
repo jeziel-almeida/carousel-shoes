3451a30d240c5c8d819fcbea1b48d3d2d3b65736
Jeziel Almeida <jeziel.almeida16@gmail.com>
2023-11-03 17:57:28 -0300

    feat: add wait functionality to carousel buttons

--- src/components/carousel/Carousel.jsx
+++ src/components/carousel/Carousel.jsx
@@ -5,17 +5,27 @@ const Carousel = ({ data }) => {
 
     const carousel = useRef(null);
     const btnLeft = useRef(null);
+    const btnRight = useRef(null);
 
     const handleLeftClick = (e) => {
         e.preventDefault();
         carousel.current.scrollLeft -= carousel.current.offsetWidth;
-        //btnLeft.current.setAttribute('disabled', true);
+        waitBtnClick();
     }
 
     const handleRightClick = (e) => {
         e.preventDefault();
         carousel.current.scrollLeft += carousel.current.offsetWidth;
-        //btnLeft.current.removeAttribute('disabled');
+        waitBtnClick();
+    }
+
+    const waitBtnClick = () => {
+        btnLeft.current.setAttribute('disabled', true);
+        btnRight.current.setAttribute('disabled', true);
+        setTimeout(() => {
+            btnLeft.current.removeAttribute('disabled');
+            btnRight.current.removeAttribute('disabled');
+        }, 500);
     }
 
     return (
@@ -41,7 +51,7 @@ const Carousel = ({ data }) => {
                 <button onClick={handleLeftClick} ref={btnLeft}>
                     <img src="/static/images/right_chevron.png" alt="Scroll left" />
                 </button>
-                <button onClick={handleRightClick}>
+                <button onClick={handleRightClick} ref={btnRight}>
                     <img src="/static/images/right_chevron.png" alt="Scroll right" />
                 </button>
             </div>
