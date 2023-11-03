ea21e558df7bba8563492f9bb250f144fccbf6ec
Jeziel Almeida <jeziel.almeida16@gmail.com>
2023-11-03 17:38:52 -0300

    feat: add carousel navigation buttons and adjust styles

--- src/components/carousel/Carousel.jsx
+++ src/components/carousel/Carousel.jsx
@@ -1,24 +1,51 @@
+import { useRef } from 'react';
 import './Carousel.css';
 
 const Carousel = ({ data }) => {
+
+    const carousel = useRef(null);
+    const btnLeft = useRef(null);
+
+    const handleLeftClick = (e) => {
+        e.preventDefault();
+        carousel.current.scrollLeft -= carousel.current.offsetWidth;
+        //btnLeft.current.setAttribute('disabled', true);
+    }
+
+    const handleRightClick = (e) => {
+        e.preventDefault();
+        carousel.current.scrollLeft += carousel.current.offsetWidth;
+        //btnLeft.current.removeAttribute('disabled');
+    }
+
     return (
-        <div className="carousel">
-            {data.map((item) => (
+        <>
+            <div className="carousel" ref={carousel}>
+                {data.map((item) => (
 
-                <div key={item.id} className="item">
-                    <div className="image">
-                        <img src={item.image} alt={item.name} />
+                    <div key={item.id} className="item">
+                        <div className="image">
+                            <img src={item.image} alt={item.name} />
+                        </div>
+
+                        <div className="info">
+                            <span className="name">{item.name}</span>
+                            <span className="oldPrice">U$ {item.oldPrice}</span>
+                            <span className="price">U$ {item.price}</span>
+                        </div>
                     </div>
 
-                    <div className="info">
-                        <span className="name">{item.name}</span>
-                        <span className="oldPrice">U$ {item.oldPrice}</span>
-                        <span className="price">U$ {item.price}</span>
-                    </div>
-                </div>
-
-            ))}
-        </div>
+                ))}
+            </div>
+            <div className='buttons'>
+                <button onClick={handleLeftClick} ref={btnLeft}>
+                    <img src="/static/images/right_chevron.png" alt="Scroll left" />
+                </button>
+                <button onClick={handleRightClick}>
+                    <img src="/static/images/right_chevron.png" alt="Scroll right" />
+                </button>
+            </div>
+        </>
     )
 }
 
